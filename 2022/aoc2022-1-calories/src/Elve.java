f51122aa337794f91cd22861a859efa340626c02
enzom-uy <enzom.dev@gmail.com>
2022-12-27 20:45:30 -0300

    Start calorie counting

---
+++ 2022/aoc2022-1-calories/src/Elve.java
@@ -0,0 +1,9 @@
+public class Elve {
+    Integer identifier;
+    Integer totalCalories;
+
+    public Elve(Integer identifier, Integer totalCalories) {
+        this.identifier = identifier;
+        this.totalCalories = totalCalories;
+    }
+}
